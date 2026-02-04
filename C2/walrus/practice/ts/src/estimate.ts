c5ccbb5842bb8b5f4356e67388436bb4227ec1c6
domenicodev <domenico.romeo@mystenlabs.com>
2026-02-04 18:05:50 +0100

    chore: Change FROST units variable name for better understanding

--- C2/walrus/practice/ts/src/estimate.ts
+++ C2/walrus/practice/ts/src/estimate.ts
@@ -4,7 +4,7 @@ import { walrus, WalrusClient } from "@mysten/walrus";
 import fs from "fs";
 
 const EPOCHS = 50;
-const FROST_TO_WAL = 1000000000;
+const FROST_PER_WAL = 1000000000;
 
 async function main() {
     // Initialize clients
@@ -21,17 +21,17 @@ async function main() {
     const mediumFileStats = await fs.promises.stat("../test_file_medium.txt");
     const {storageCost, writeCost, totalCost} = await walrusClient.storageCost(mediumFileStats.size, EPOCHS);
     console.log(`Medium File (${mediumFileStats.size / 1024} KB):`);
-    console.log(`Storage Cost: ${Number(storageCost) / FROST_TO_WAL} WAL`);
-    console.log(`Write Cost: ${Number(writeCost) / FROST_TO_WAL} WAL`);
-    console.log(`Total Cost: ${Number(totalCost) / FROST_TO_WAL} WAL`);
+    console.log(`Storage Cost: ${Number(storageCost) / FROST_PER_WAL} WAL`);
+    console.log(`Write Cost: ${Number(writeCost) / FROST_PER_WAL} WAL`);
+    console.log(`Total Cost: ${Number(totalCost) / FROST_PER_WAL} WAL`);
 
     // Small File
     const smallFileStats = await fs.promises.stat("../test_file_small.txt");
     const {storageCost: smallStorageCost, writeCost: smallWriteCost, totalCost: smallTotalCost} = await walrusClient.storageCost(smallFileStats.size, EPOCHS);
     console.log(`\nSmall File (${smallFileStats.size / 1024} KB):`);
-    console.log(`Storage Cost: ${Number(smallStorageCost) / FROST_TO_WAL} WAL`);
-    console.log(`Write Cost: ${Number(smallWriteCost) / FROST_TO_WAL} WAL`);
-    console.log(`Total Cost: ${Number(smallTotalCost) / FROST_TO_WAL} WAL`);
+    console.log(`Storage Cost: ${Number(smallStorageCost) / FROST_PER_WAL} WAL`);
+    console.log(`Write Cost: ${Number(smallWriteCost) / FROST_PER_WAL} WAL`);
+    console.log(`Total Cost: ${Number(smallTotalCost) / FROST_PER_WAL} WAL`);
 }
 
 main();
